d89799264cf359ba9b089dc36c4bfd11645edf80
GasGas7 <79295810+GasGas7@users.noreply.github.com>
2021-07-10 16:41:34 +0200

    add links merch to main - to do sass style

---
+++ config/links_dc.php
@@ -0,0 +1,29 @@
+<?php 
+
+return [
+    [
+        'path_image'=>"/img/buy-comics-digital-comics.png",
+        'link'=> "digital comics"
+    
+    ],
+    [
+        'path_image'=>"/img/buy-comics-merchandise.png",
+        'link'=> "dc merchandise"
+    
+    ],
+    [
+        'path_image'=>"/img/buy-comics-subscriptions.png",
+        'link'=> "subscription"
+    
+    ],
+    [
+        'path_image'=>"/img/buy-comics-shop-locator.png",
+        'link'=> "comic shop locator"
+    
+    ],
+    [
+        'path_image'=>"/img/buy-dc-power-visa.svg",
+        'link'=> "dc power visa"
+    
+    ]
+];
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,23 +5,41 @@
     <div class="comics_main">
 
         <div class="comics_list">
-            
+
             <div class="container-xl p-0 d-flex flex-wrap pos_rel_container">
                 <div class="button_top">CURRENT SERIES</div>
                 <div class="row">
                     @foreach ($comics as $index => $comic)
-                            <div class="card_comic col-2 p-2">
-                                <div class="img_comic">
-                                    <img class="img-fluid" src="{{ $comic['thumb'] }}" alt="">
-                                </div>
-                                <a href="{{route('comic',['id' => $index])}}">
-                                    <p>{{ $comic['series'] }}</p>
-                                </a>
-                            </div> 
+                        <div class="card_comic col-2 p-2">
+                            <div class="img_comic">
+                                <img class="img-fluid" src="{{ $comic['thumb'] }}" alt="">
+                            </div>
+                            <a href="{{ route('comic', ['id' => $index]) }}">
+                                <p>{{ $comic['series'] }}</p>
+                            </a>
+                        </div>
                     @endforeach
                     <a class="button_a_bottom">LOAD MORE</a>
                 </div>
+
+
             </div>
+            <div class="container-xl">
+
+                <div class="row">
+                    <div class="dc_links d-flex justify-content-between mt-3 mb-3 " style="width: 100%;">
+                        @foreach (config('links_dc') as $item)
+                            <div class="d-flex justify-content-around align-items-center">
+                                <img src="{{ $item['path_image'] }}" style="height:70%" alt="">
+                                <p style="color: green">{{ $item['link'] }}</p>
+                            </div>
+
+                        @endforeach
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     </div>
 
